c40170560531b8f62c96f6f476af6606991eb857
uday-nitjsr <uday.nitjsr.451@gmail.com>
2018-05-10 14:17:53 +0530

    adding postman collection

--- cypress/integration/conduitExample/homepage_spec.js
+++ cypress/integration/conduitExample/homepage_spec.js
@@ -1,4 +1,4 @@
-describe("This is to home page when logged in",function(){
+describe("This is to test home page when logged in",function(){
 	beforeEach(function(){
 		cy.loginConduit("jake@jake.jake","jakejake")
 	})
@@ -57,6 +57,94 @@ describe("This is to home page when logged in",function(){
 		cy.get("ul.nav-pills a.nav-link")
 		.contains("Global Feed")
 		.should("be.visible")
+	})
+
+	it("verify sidebar labels",function(){
+		cy.visit("/")
+
+		cy.get("div.sidebar>p")
+		.should("have.text","Popular Tags")
+		.should("be.visible")
+	})
+
+	it("verify all tags are present",function(){
+		var allTags
+		cy.request("http://localhost:3000/api/tags")
+		.then((response)=>{
+			allTags = response.body.tags
+		})
+		cy.visit("/")
+
+		cy.get("div.tag-list>a")
+		.each(function($ele,index){
+			cy.wrap($ele)
+			.should("have.text",allTags[index])
+		})
+	})
+
+	it("verify articles on Global feeds",function(){
+		var allArticles
+		cy.request("http://localhost:3000/api/articles?limit=10&offset=0")
+		.then((response)=>{
+			allArticles = response.body.articles		
+		})
+
+		cy.visit("/")
+
+		cy.get("ul.nav-pills a.nav-link")
+		.contains("Global Feed")
+		.click()
+		.should("have.class","active")
+
+		cy.get("div.article-preview")
+		.should("be.visible")
+		.each(function($ele,index){
+			cy.wrap($ele)
+			.find("a.preview-link>h1")
+			.should("be.visible")
+			.should("have.text",allArticles[index].title)
+			
+			cy.wrap($ele)
+			.find("a.preview-link")
+			.should("have.attr","href","/article/"+allArticles[index].slug)
+
+			cy.wrap($ele)
+			.find("a")
+		})
+	})
+})
+
+describe("This is to test when user is NOT logged in",function(){
+	it("verify headers",function(){
+		cy.visit("/")
+
+		cy.get("ul.nav-pills a.nav-link")
+		.should("be.visible")
+
+		cy.get("ul.navbar-nav a.nav-link")
+		.contains("Home")
+		.should("have.attr","href","/")
+
+		cy.get("ul.navbar-nav a.nav-link")
+		.contains("Sign in")
+		.should("have.attr","href","/login")
+
+		cy.get("ul.navbar-nav a.nav-link")
+		.contains("Sign up")
+		.should("have.attr","href","/register")
+	})
+
+	it("verify feeds header",function(){
+		cy.visit("/")
+
+		cy.get("ul.nav-pills a.nav-link")
+		.its("length")
+		.should("eq",1)
+
+		cy.get("ul.nav-pills a.nav-link")
+		.contains("Global Feed")
+		.should("be.visible")
+		.should("have.class","active")
 	})
 
 	it("verify sidebar labels",function(){
